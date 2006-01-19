1f9a66a2cc59d8197187271a24e542b334a01071
adinardi <adinardi@e1c6a677-3c05-0410-8139-d630aaec0cf4>
2006-01-19 06:43:03 +0000

    here.
    git-svn-id: svn+ssh://rancor/users/u9/drinksvr/svn/newDrink/tini@28 e1c6a677-3c05-0410-8139-d630aaec0cf4

---
+++ OneWireCommands.java
@@ -0,0 +1,14 @@
+/*
+ * OneWireCommands.java
+ *
+ * 
+ *
+ */
+
+/**
+ *This is the fun class that will handle 1-wire commands on the tinis 
+ *@author Angelo DiNardi
+ */
+public class OneWireCommands {
+    //Stuff here.
+}
