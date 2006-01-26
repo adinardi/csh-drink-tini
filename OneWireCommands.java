ff928efd5fe9b2e39d3425aead251c462ae48eca
adinardi <adinardi@e1c6a677-3c05-0410-8139-d630aaec0cf4>
2006-01-26 18:49:01 +0000

    did more stubbing and such.
    git-svn-id: svn+ssh://rancor/users/u9/drinksvr/svn/newDrink/tini@29 e1c6a677-3c05-0410-8139-d630aaec0cf4

--- OneWireCommands.java
+++ OneWireCommands.java
@@ -10,5 +10,33 @@
  *@author Angelo DiNardi
  */
 public class OneWireCommands {
+    private DSPortAdapter adapter = null;
     //Stuff here.
+    public DSPortAdapter() {
+        this.adapter = OneWireAccessProvider.getDefaultAdapter();
+    }
+
+    public int drop ( int slot ) {
+        //check if slot is empty
+        if( isEmpty( slot ) ) {
+            //return empty code
+        } 
+        //grab exclusive
+       
+        //fire switch
+        //do the 2 second wait
+        //unfire switch
+        
+        //close adapter
+        //check is empty for status?
+        return 0; 
+    }
+
+    public boolean isEmpty( int slot ) {
+        
+    }
+    
+    private OneWireContainer05 getSwitch( long id ) {
+        return this.adapter.getDeviceContainer( id );
+    }
 }
